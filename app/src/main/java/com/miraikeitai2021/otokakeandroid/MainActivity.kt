ebcc5d4319943728204886d635929484de1784ee
b1019116 <softshiro2009@yahoo.co.jp>
2021-09-20 21:56:45 +0900

    first commit

---
+++ app/src/main/java/com/miraikeitai2021/otokakeandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.miraikeitai2021.otokakeandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
