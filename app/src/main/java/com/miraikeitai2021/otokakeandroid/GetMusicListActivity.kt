87b7bd4b29715a36af5e9a9929a2ca1be8f855eb
b1019116 <softshiro2009@yahoo.co.jp>
2021-11-03 15:02:05 +0900

    music file download work in progress

--- app/src/main/java/com/miraikeitai2021/otokakeandroid/GetMusicListActivity.kt
+++ app/src/main/java/com/miraikeitai2021/otokakeandroid/GetMusicListActivity.kt
@@ -1,12 +1,17 @@
 package com.miraikeitai2021.otokakeandroid
 
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
+import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.httpDownload
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
 import kotlinx.coroutines.Dispatchers
@@ -18,12 +23,15 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonTransformingSerializer
+import java.io.File
+import java.io.InputStream
 
 /**
  * サーバから，曲一覧を取得するAPIにアクセスして，登録されている曲の情報を排列で取得するActivity.
  * 今は仮版としてActivityとして実装しているが，いずれは長谷川君のPlaylisActivityに合わせることになる．
  * */
 class GetMusicListActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_music_list)
@@ -32,9 +40,11 @@ class GetMusicListActivity : AppCompatActivity() {
         val musicListRepository = MusicListRepository()
         val musicListViewModel = MusicListViewModel(musicListRepository)
         musicListViewModel.getMusicList()
-    }
 
-    private inner class MusicRecyclerViewAdapter(private val dataList: Array<MusicInfo>)
+        findViewById<Button>(R.id.download_music_button).setOnClickListener {
+            musicListViewModel.downloadMusic()
+        }
+    }
 }
 
 @Serializable
@@ -80,6 +90,22 @@ class MusicListViewModel(private val musicListRepository: MusicListRepository): 
             }
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun downloadMusic() {
+        viewModelScope.launch(Dispatchers.Main) {
+            try {
+                val httpResult = musicListRepository.requestDownloadMusic()
+                when (httpResult){
+                    is HttpResult.Success<InputStream> -> {
+
+                    }
+                }
+            }catch(e: Exception){
+                Log.e("debug", e.toString())
+            }
+        }
+    }
 }
 
 sealed class HttpResult<out R>{
@@ -114,6 +140,34 @@ class MusicListRepository(){
                     }catch(e: Exception){
                         Log.e("debug" , e.toString())
                         httpResult = HttpResult.Error(e)
+                    }
+                }
+            }
+        }
+        return httpResult
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    suspend fun requestDownloadMusic(): HttpResult<InputStream> {
+        val url = "https://testotokake.s3.ap-northeast-1.amazonaws.com/music/maoudamashii-halzion.mp3"
+        var httpResult: HttpResult<InputStream> = HttpResult.Error(Exception("No http request was executed."))
+        withContext(Dispatchers.IO){
+            url.httpDownload().fileDestination { response, url ->
+                File.createTempFile("temp", ".tmp")
+            }.progress{ readBytes, totalBytes ->
+                val progress = readBytes.toFloat() / totalBytes.toFloat()
+                Log.d("debug", "current progress: $progress");
+            }.response{ res, req, result ->
+                when(result){
+                    is Result.Failure -> {
+                        val ex = result.getException()
+                        Log.e("debug", ex.toString())
+                        httpResult = HttpResult.Error(Exception(ex.toString()))
+                    }
+                    is Result.Success -> {
+                        val inputStream = result.value.inputStream()
+                        Log.d("debug", "downloaded data is: $inputStream");
+                        httpResult = HttpResult.Success<InputStream>(inputStream)
                     }
                 }
             }
