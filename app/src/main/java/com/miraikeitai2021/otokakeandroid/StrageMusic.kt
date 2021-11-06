b3830da34f33cd7c6b3fb4a253caced019f49d13
b1019116 <softshiro2009@yahoo.co.jp>
2021-11-06 13:17:22 +0900

    discard my changes in StrageMusic.kt

--- app/src/main/java/com/miraikeitai2021/otokakeandroid/StrageMusic.kt
+++ app/src/main/java/com/miraikeitai2021/otokakeandroid/StrageMusic.kt
@@ -6,13 +6,12 @@ import android.os.Build
 import android.provider.MediaStore
 import androidx.annotation.RequiresApi
 import java.io.FileOutputStream
-import java.io.InputStream
 
 class StrageMusic {
-/**
- * ストレージに曲データを保存する関数
- * context: MainActivity（呼び出し元Activity)のContext
- */
+    /**
+     * ストレージに曲データを保存する関数
+     * context: MainActivity（呼び出し元Activity)のContext
+     */
 
     @RequiresApi(Build.VERSION_CODES.Q)
 
@@ -29,37 +28,6 @@ class StrageMusic {
         context.contentResolver.openFileDescriptor(item, "w", null).use{
             FileOutputStream(it!!.fileDescriptor).use{ outputStream ->
                 val audioInputStream = context.resources.openRawResource(R.raw.natsuyasuminotanken) //保存する曲ファイルをローカルフォルダから指定
-                while(true){
-                    val data = audioInputStream.read()
-                    if(data == -1){
-                        break
-                    }
-                    outputStream.write(data)
-                }
-                audioInputStream.close()
-                outputStream.close()
-            }
-        }
-
-        values.clear()
-        values.put(MediaStore.Audio.Media.IS_PENDING, 0)
-        context.contentResolver.update(item, values, null, null)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.Q)
-    fun StrageInMusicWithInputStream(context: Context, audioInputStream: InputStream){
-
-        val values= ContentValues().apply {
-            put(MediaStore.Audio.Media.DISPLAY_NAME, "testMusic.mp3") //保存する曲ファイルの名前を入力
-            put(MediaStore.Audio.Media.IS_PENDING, 1)
-        }
-
-        val collection = MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-        val item = context.contentResolver.insert(collection, values)!!
-
-        context.contentResolver.openFileDescriptor(item, "w", null).use{
-            FileOutputStream(it!!.fileDescriptor).use{ outputStream ->
-//                val audioInputStream = context.resources.openRawResource(R.raw.natsuyasuminotanken) //保存する曲ファイルをローカルフォルダから指定
                 while(true){
                     val data = audioInputStream.read()
                     if(data == -1){
